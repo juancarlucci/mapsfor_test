9fd88a7241cadc428040c6881f0e95edd2f2ba45
Juan Carlos <juancarlucci@users.noreply.github.com>
2017-04-18 19:40:57 -0700

    Update using Goole Sheets ID

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 var constants = {
-	googleDocID: '1I1bHQTUNCPHD6AuyNQfYV6g0qqJI8OjF9UHP9MW4XYg',
+	googleDocID: '1o26zvhu_HH7LoyfVCFMC0FJnCj6hwF7yW2D5hTItD1k',
 	informationSheetName: 'Information',
 	pointsSheetName: 'Points'
 };
