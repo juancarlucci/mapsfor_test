0fba43851faa926b82b0e87ad47769d54d92bdbc
Juan Carlos <juancarlucci@users.noreply.github.com>
2017-05-03 15:45:38 -0700

    Update attribution

--- map.js
+++ map.js
@@ -154,7 +154,7 @@ window.onload = function () {
 
     var attributionHTML = document.getElementsByClassName("leaflet-control-attribution")[0].innerHTML;
     var mapCreatorAttribution = documentSettings["Your Name:"] === '' ? 'Built with' : 'This map was built by ' + documentSettings["Your Name:"] + ' using';
-    attributionHTML = mapCreatorAttribution + attributionHTML;
+    attributionHTML = mapCreatorAttribution + '<a href="http://mapsfor.us/">mapsfor.us</a><br>' attributionHTML;
     document.getElementsByClassName("leaflet-control-attribution")[0].innerHTML = attributionHTML;
   }
 };
